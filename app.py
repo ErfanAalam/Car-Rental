308c6253d1eee906331731aa3822a715ecfc2263
lokendar jangid <lokendarjangid1234@gmail.com>
2024-02-29 18:07:49 +0530

    new commits

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api', methods=['POST'])
+@app.route('/')
 def api():
-    data = request.get_json()
-    return jsonify(data)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
