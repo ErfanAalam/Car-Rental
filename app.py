faf39f37a297202d4afa5e1d31e9b2b1e5bcd888
lokendar jangid <lokendarjangid1234@gmail.com>
2024-03-02 23:59:18 +0530

    converted all code to flask

--- app.py
+++ app.py
@@ -3,8 +3,34 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 @app.route('/')
-def api():
+def home():
     return render_template('index.html')
+
+# Uncomment the following route and rename the function if needed
+# to avoid conflicts with other routes.
+# @app.route('/testimonial')
+# def testimonial():
+#     return render_template('testimonial.html')
+
+# packages
+@app.route('/packages')
+def packages():
+    return render_template('packages.html')
+
+# gallery
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+# about us 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# contact us
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
