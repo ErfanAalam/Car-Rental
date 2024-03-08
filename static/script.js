f0c0af29a758b921dd401404d32e8709098df273
ErfanAalam <efankhansiwani@gamil.com>
2024-03-08 22:01:27 +0530

    hamburger fixed

--- static/script.js
+++ static/script.js
@@ -3,8 +3,8 @@ const features = document.querySelector(".features");
 const close = document.querySelector(".close")
 
 hamburger.addEventListener("click",()=>{
-    if (features.classList.contains("translate-x-96")) {
-        features.classList.remove("translate-x-96");
+    if (features.classList.contains("translate-x-[1200px]")) {
+        features.classList.remove("translate-x-[1200px]");
     }
 })
 hamburger.addEventListener("click",()=>{
@@ -15,7 +15,7 @@ close.addEventListener("click",()=>{
 })
 
 close.addEventListener("click",() =>{
-    features.classList.add("translate-x-96")
+    features.classList.add("translate-x-[1200px]")
 })
 
 // image slider component
